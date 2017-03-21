26538bf1a655982e7802db3552b348e216fa56b9
k4ndAr3c <k4ndAr3c@github.com>
2017-03-21 12:22:25 +0100

    alias ++

--- alias.zsh
+++ alias.zsh
@@ -13,7 +13,7 @@ alias a2='/etc/init.d/apache2 start'
 alias a2s='/etc/init.d/apache2 stop'
 alias a2s='systemctl stop httpd.service'
 alias a2='systemctl start httpd.service'
-alias a='apt'
+alias a='apt-get'
 alias A='aptitude search'
 alias adg='apt dist-upgrade'
 alias adg='dnf distribution-synchronization'
@@ -97,7 +97,7 @@ alias du='du -h'
 alias e='cd /DATAS/ ; clear_console'
 alias E='exit'
 alias egrep='egrep --color=auto'
-alias EPR='time emerge --ignore-default-opts --keep-going -v @preserved-rebuild'
+alias EPR='FEATURES="-getbinpkg" emerge --ignore-default-opts -v --keep-going @preserved-rebuild -a'
 alias eq="equery"
 alias f3doR="/usr/bin/sshXKey 10.42.1.18"
 alias f3doR="/usr/bin/sshXKey root@10.42.1.18"
@@ -131,6 +131,7 @@ alias grep='grep --color=auto'
 alias GROLAND="youtube-dl 'http://www.canalplus.fr/c-humour/pid1787-c-groland.html'"
 alias -g triPkgList="cut -d'(' -f2|rmVer|sort -u|uniq"
 alias GUIGNOLS="youtube-dl 'http://www.canalplus.fr/c-humour/pid1784-c-les-guignols.html'"
+alias GUp="GITs.py /pentest/PeNtEsT /root"
 alias H='cat /root/.zsh_history | grep -i '
 alias h='cd /home/k4ndar3c/ ; clear'
 alias HEAders="wget -S --spider -O - "
@@ -143,6 +144,8 @@ alias I='iwconfig'
 alias IpForward='echo 1 > /proc/sys/net/ipv4/ip_forward'
 alias IpForwardStop='echo 0 > /proc/sys/net/ipv4/ip_forward'
 alias IPv6='nmap -6 --script="*ipv6*" --script-args=newtargets'
+alias ircNewbieCon="irssi -c irc.worldnet.net"
+alias ircRootMe="irssi -c irc.root-me.org"
 alias jaR0="/usr/bin/sshXKey 10.42.1.10"
 alias jaR0="/usr/bin/sshXKey root@10.42.1.10"
 alias j='jobs'
@@ -199,6 +202,7 @@ alias NMStart="systemctl start NetworkManager.service"
 alias NMStop="service network-manager stop;pkill wpa_supplicant"
 alias NMStop="systemctl stop NetworkManager.service wpa_supplicant.service"
 alias n='netstat -antup ; echo ; ss -antup ; echo ; lsof -i -P'
+alias NoBinpkg='FEATURES="-getbinpkg"'
 alias np='netstat -anp | grep -i'
 alias nSTool="/usr/bin/sshXKey 10.42.1.34"
 alias nt='arp-scan -l -I'
@@ -212,9 +216,10 @@ alias p2='/etc/init.d/postgresql start'
 alias p2s='/etc/init.d/postgresql stop'
 alias p2s='systemctl stop postgresql'
 alias p2='systemctl start postgresql'
+alias penShare='mount p3nt00:/home/k4ndar3c /mnt/pent00'
 alias pentoo="/usr/bin/sshXKey 10.42.1.29"
 alias pentoo="/usr/bin/sshXKey root@10.42.1.29"
-alias penUp2='time emerge --color y -uDNpv --alphabetical --with-bdeps y @system @world'
+alias penUp2='time emerge --color y -uDNav --alphabetical --with-bdeps y @system @world'
 alias penUp='agSmooth portage ; time emerge --deep --update --newuse -kb --changed-use --newrepo @world'
 alias PipUpgradeAll="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
 alias poogle='ping google.com -c 5 ; ping google.fr -c 5'
@@ -255,6 +260,9 @@ alias SKR="ssh-keygen -R "
 alias slax="/usr/bin/sshXKey 10.42.1.23"
 alias slax="/usr/bin/sshXKey root@10.42.1.23"
 alias sm00th="ssh -X -i /home/k4ndar3c/KEY -p 222 10.42.1.5"
+alias smb03="smbclient \\\\\\\\10.42.1.25\\\\SHARE03 -U Administrateur"
+alias smb12="smbclient \\\\\\\\10.42.1.14\\\\d0wn5 -U Administrator"
+alias smbW7="smbclient \\\\\\\\10.42.1.33\\\\FILES -U k4ndar3c"
 alias SolvFilConfl="FEATURES='-collision-detect -protect-owned'"
 alias Spike="/usr/bin/sshXKey 10.42.1.22"
 alias Spike="/usr/bin/sshXKey root@10.42.1.22"
@@ -291,6 +299,7 @@ alias VU="vim /etc/portage/package.use"
 alias VW="vim /var/lib/portage/world"
 alias WEBsecurify="cd /EXT4/SVNs/websecurify-read-only && ./xulrunner9.0/xulrunner --app ./xulrunner/websecurify/applications/scanner/application.ini &"
 alias whichSong="ssh root@10.42.1.19 lsof -c vlc|grep Z1C|cut -d '/' -f4,5,6,7,8,9,10,11,12,13,14"
+alias win12="rdesktop -k fr -u Administrator -p 'W1Nddos)' -g 1024x1024 10.42.1.14 &"
 alias WLAN0scan="iw wlan0 scan |egrep -ie '(SSID|signal)'"
 alias WLAN1scan="iw wlan1 scan |egrep -ie '(SSID|signal)'"
 alias WLAN2scan="iw wlan2 scan |egrep -ie '(SSID|signal)'"
@@ -308,22 +317,28 @@ function Append0-9 { for i in $(cat $1) ; do seq -f $i%1.0f 0 9 ; done ; }
 function CHROOT { mount $1 $2 ; mount -t proc /proc $2/proc ; mount -t sysfs /sys $2/sys ; mount -B /dev $2/dev ; mount -B /dev/pts $2/dev/pts ; chroot $2 ; }
 function GACP { git add --all && git commit -a -m $1 && git push -u origin $2 ; }
 function GMD5 { egrep $1 /pentest/PeNtEsT/pentestscr1pts/w0Rdl1stS/MD5s-resolv.lst ; }
+function hexMd5sum { echo -n $1 | xxd -r -p | md5sum ; }
 function live-clean { for i in {1..$1} ; do nmcli c delete "$2 $i" ; done ; }
 function LookAtGit { elinks "https://github.com/$1?tab=repositories" ; }
+function lsbTestAll { for i in mersenne log_gen identity fibonacci fermat eratosthenes_composite eratosthenes carmichael ackermann OEIS_A000217 Dead_Man_Walking syracuse ; do lsb-set reveal -i $1 -g $i ; done ; }
 function MAC { for i in $(ifconfig -a|grep flag|grep -v lo |cut -f1 -d":") ; do echo ; echo $i ; macchanger -s $i ; done ; }
 function MAC { for i in $(ifconfig -a | grep Link | awk '{print $1}' | grep -v lo) ; do echo ; echo $i ; macchanger -s $i ; done ; }
-function MakePlayList { touch ./"$2" && find "$1" -print | egrep -ie '(*.mp3|*.wav|*.m4a|*.wma|*.ogg|*.mp4|*.mpc)'|sed 's/^/\"/g' |sed 's/$/\"/g'  >> "$2" ; echo ---DONE!--- ; }
+function MakePlayList { touch ./"$2" && find "$1" -print | egrep -ie '(*.mp3|*.wav|*.m4a|*.wma|*.ogg|*.mp4|*.mpc|*.mp4a)'|sed 's/^/\"/g' |sed 's/$/\"/g'  >> "$2" ; echo ---DONE!--- ; }
+function makeShellCode { nasm -f $2 $1.asm -o $1.o ; for i in $(objdump -d $1.o|grep "^ "|cut -f2); do echo -n '\\x'$i ; done ; echo ; }
 function nmapSearchX { nmap -sV -oX /tmp/n.xml $1 ; searchsploit --nmap /tmp/n.xml ; }
 function NP { netstat -anp | grep -i $1 ; ss -anp | grep -i $1 ; lsof -i -P | grep -i $1 ; }
 function PdfDim { gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=$1 $2 ; }
 function Qf { find . -iname "*$1*" ; }
 function Q { for i in premiere deuxieme ; do youtube-dl -f http-705 http://www.tf1.fr/tmc/quotidien-avec-yann-barthes/videos/quotidien-$i-partie-$1-$2-2017.html || youtube-dl -f http-704 http://www.tf1.fr/tmc/quotidien-avec-yann-barthes/videos/quotidien-$i-partie-$1-$2-2017.html ; done ; }
+function readelfSort { readelf -s $1 | awk '{print $2,$8}' | sort ; }
 function RedirPort { iptables -t nat -A PREROUTING -i $3 -p tcp --dport $1 -j REDIRECT --to-port $2 ; }
 function ReVerse { python3 -c "print(''.join(reversed(\"$1\")))" ; }
 function ReVerse { python -c "print(''.join(reversed(\"$1\")))" ; }
 function scpKEY { scp -i /home/k4ndar3c/KEY $1 $2 ; }
+function shift51 { for i in {1..51} ; do shiftCipher3.py $1 $i ; done ; for i in {-1..-51} ; do shiftCipher3.py $1 $i ; done ; }
 function showPkgsConfig { for i in `ls /etc/portage/package*` ; do echo -e '\n'$i'#######' ; cat $i ; done ; }
 function SshForAll { for i in $(arp-scan -l |grep 10\.42|grep -v \.100 |cut -f 1) ;do echo $i ; ssh $i $* ; echo '\n';done ;ssh 10.42.0.1 $* }
 function SshForAll { for i in $(/usr/bin/arp-scan -l |grep 10\.42|grep -v \.100 |cut -f 1) ;do echo $i ; ssh $i $* ; echo '\n';done ;ssh 10.42.0.1 $* ; }
+function VirusTotal { python -c "import requests;print(requests.post('https://www.virustotal.com/en/search/', data={'query':'$1'}).content)" |egrep -i '57 antivirus' ; }
 function WHILe { while sleep $1 ; do $2 $3 $4 $5 $6 $7 $8 $9 ; done ; }
 function WHO { ripdc.sh -t $1 ; echo "-----------------" ; nslookup $1 208.67.222.222 ; echo "-----------------" ; dig -t ANY $1 ; echo "-----------------" ; host -a $1 8.8.8.8 ; echo "-----------------" ; dmitry $1 ; }
